6dd46525d5f97f3ee7c2a1b83683c06867d04e2f
erickgato <itsbk0703@gmail.com>
2020-07-26 17:44:26 -0300

    DDD route

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,14 @@
 const routes = require('express').Router();
+const DDDcontroller = require('./controllers/dddController');
+//DDDs
+routes.get('/ddds', DDDcontroller.index );
+routes.post('/ddds', DDDcontroller.store);
+routes.delete('/ddds', DDDcontroller.Delete);
+routes.put('/ddds', DDDcontroller.Alter);
 
+
+
+
+
+//Export
 module.exports = routes;
