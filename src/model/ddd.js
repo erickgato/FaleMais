58644c4b79d32be54ad433afe3b8295e28f6822a
erickgato <itsbk0703@gmail.com>
2020-07-26 17:44:09 -0300

    Model crud

--- src/model/ddd.js
+++ src/model/ddd.js
@@ -1,17 +1,48 @@
-const knex = require('../database/database');
+const knex = require("../database/database");
 
-class DddModel{
-    async GetDDDs() {
-        const DDD  =  await knex('fm_ddd');
-        return DDD;
-    }
-    async GetDDD(number) {
-        const result = await knex.select('*').from('fm_ddd').where('ddd','011');
-        return result;
-    }
-    async CreateDDD(ddd) {
-        const insert = await knex.insert([{ddd}]).into('fm_ddd');
-        return insert;
-    }
+class DddModel {
+  async GetDDDs() {
+    const DDD = await knex("fm_ddd");
+    return DDD;
+  }
+  async GetDDD(number) {
+    const result = await knex
+      .select("*")
+      .from("fm_ddd")
+      .where("ddd", number)
+      .catch((err) => {
+        "Error while searching ddd";
+      });
+    return result;
+  }
+  async Create(ddd) {
+    const insert = await knex
+      .insert([{ ddd }])
+      .into("fm_ddd")
+      .catch((err) => {
+        return "Error while creating ddd";
+      });
+    return insert;
+  }
+  async Delete(number) {
+    const del = await knex("fm_ddd")
+      .where("ddd", number)
+      .del()
+      .then(() => "Sucess deleting")
+      .catch(() => "Error while deleting");
+    return del;
+  }
+
+  async Alter(id, number) {
+    return await knex("fm_ddd")
+      .where({ ddd_id: id })
+      .update({
+        ddd: number,
+      })
+      .then(() => {
+        "Updated sucess";
+      })
+      .catch(() => "Error while updating");
+  }
 }
 module.exports = new DddModel();
