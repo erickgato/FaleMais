4e633237b27d93ba89ae86efe5b33e45107e0f3a
erickgato <itsbk0703@gmail.com>
2020-07-31 09:24:40 -0300

    Teste de planos

--- __tests__/integration/DDD.test.js
+++ __tests__/integration/DDD.test.js
@@ -8,9 +8,24 @@ describe('Test ddd controller actions', () => {
     .post('/ddds')
     .send(`ddd=${DDD}`);
 
-    expect(response.status).toBe(201);
+   await expect(response.status).toBe(201);
   })
   it(' should return ddd ', async () => {
-      
+      const Send = await request(app)
+      .get('/ddds')
+      await expect(Send).not.toBe('');
   })
+  it(' should return one ddd ', async () => {
+    const Send = await request(app)
+    .get('/ddds')
+    .send(`ddd=${DDD}`);
+    await expect(Send.status).not.toBe(404);
+})
+  it( ' shold delete ddd', async () => {
+    const response = await request(app)
+    .delete('/ddds')
+    .send(`ddd=${DDD}`);
+
+   await expect(response.status).toBe(200);
+  } )
 })
