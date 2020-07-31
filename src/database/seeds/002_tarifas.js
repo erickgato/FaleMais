d806b579631946e119d535379b27b05b79d9f435
erickgato <itsbk0703@gmail.com>
2020-07-31 12:32:42 -0300

    Added more seeds

--- src/database/seeds/002_tarifas.js
+++ src/database/seeds/002_tarifas.js
@@ -4,6 +4,11 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('fm_tarifa').insert([
         {ddd_origin: 1, ddd_destination: 2, H_M: '1.90'},
+        {ddd_origin: 1, ddd_destination: 1, H_M: '0.10'},
+        {ddd_origin: 2, ddd_destination: 2, H_M: '0.10'},
+        {ddd_origin: 3, ddd_destination: 3, H_M: '0.10'},
+        {ddd_origin: 4, ddd_destination: 4, H_M: '0.10'},
+        {ddd_origin: 4, ddd_destination: 2, H_M: '1.99'},
         {ddd_origin: 2, ddd_destination: 1, H_M: '2.90'},
         {ddd_origin: 1, ddd_destination: 3, H_M: '1.70'},
         {ddd_origin: 3, ddd_destination: 1, H_M: '2.70'},
